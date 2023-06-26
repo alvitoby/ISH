8cd80bb050e5444665e79bb9d469edcb75b9c220
unknown <yosobumi@gmail.com>
2023-06-26 14:22:11 +0700

    commit pertama

---
+++ storage/framework/views/e5cf7270b900384cbfe9819c3ac2fa8c.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+
+<head>
+  <html>
+  <title>Training</title>
+  <link rel="stylesheet" href="css/style.css" />
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    integrity="sha512-UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" /> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+  <div class="fContainer">
+    <!-- //navigation -->
+
+    <nav class="wrapper">
+      <ul class="navigation">
+        <li><a style="padding: 50px" href="">About</a></li>
+        <li><a style="padding: 50px">Products</a></li>
+        <li><a style="padding: 50px">Careers</a></li>
+      </ul>
+      <div class="brand">
+        <img src="images/ishlogo.png" style="width: 100px; height: 100" />
+      </div>
+      <ul class="navigation">
+        <li><a style="padding: 36px">ISH News</a></li>
+        <li><a style="padding: 36px">Our Clients</a></li>
+        <li><a style="padding: 36px">Contact US</a></li>
+      </ul>
+      <div class="toggle_btn">
+        <i class="fa-solid fa-bars"></i>
+      </div>
+    </nav>
+  </div>
+
+  <div class="dropdown_menu">
+    <li><a style="padding: 50px" href="">About</a></li>
+    <li><a style="padding: 50px">Products</a></li>
+    <li><a style="padding: 50px">Careers</a></li>
+    <li><a style="padding: 36px">ISH News</a></li>
+    <li><a style="padding: 36px">Our Clients</a></li>
+    <li><a style="padding: 36px">Contact US</a></li>
+  </div>
+  <div class="row">
+    <div>
+      <img src="images/ig.png" alt="" />
+      <img src="images/fb.png" alt="" />
+      <img src="images/in.png" alt="" />
+    </div>
+    <h1 style="font-weight: 600;">Home</h1>
+    <button class="btn-job">NEED A JOB ?</button>
+  </div>
+  <!-- Carousel -->
+  <div class="zzsize">
+    <div class="group">
+      <div class="item">
+        <img src="images/ish1.gif" alt="" class="gambar" />
+      </div>
+      <div class="item">
+        <img src="images/ish2.jpg" alt="" class="gambar" />
+      </div>
+      <div class="item">
+        <img src="images/ish3.jpg" alt="" class="gambar" />
+      </div>
+      <div class="item">
+        <img src="images/ish4.jpg" alt="" class="gambar" />
+      </div>
+      <div class="item">
+        <img src="images/ish5.jpg" alt="" class="gambar" />
+      </div>
+
+
+      <!-- <div class="item" style="background-image: url(images/char6.jpg)"></div> -->
+    </div>
+  </div>
+
+
+  <div class="zzsize">
+    <div class="group2">
+      <img class="" src="images/orang.png" alt="" />
+      <div>
+        <div class="text-container">
+          <div>
+            <h1>
+              <img class="hr" src="images/HR.png" alt="" />Welcome to <br />
+              <span class="ish-text">ISH</span>
+            </h1>
+            <p>
+              PT Infomedia Solusi Humanika atau dikenal dengan ISH yang
+              berdiri pada tanggal 24 Oktober 2012 merupakan anak perusahaan
+              PT Infomedia Nusantara (bagian dari Telkom Grup) yang bergerak
+              dalam bidang Human Capital Services. ISH telah mampu memberikan
+              solusi layanan pengelolaan SDM terbaik kepada lebih dari 164
+              perusahaan yang tersebar di 420 kota dan mengelola lebih dari
+              13.000 karyawan di seluruh Indonesia.
+            </p>
+            <button class="abt-btn">Tentang Perusahaan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Why  -->
+  <div class="divcoba" style="margin-top: 1800px;"></div>
+  <!-- <div class="spacer"></div> -->
+  <div class="divcoba2" style="margin-top: 1800px;"></div>
+  <div class="" style="padding-top: 150px; text-align: center; font-size: 36px;margin-left: 10%;
+         margin-right: 10%;
+         margin-bottom: 8%;">
+    <h1 class="ish-text2">Why ISH?</h1>
+  </div>
+
+  <div class="zzsize">
+    <div>
+      <div style="display: flex;">
+        <div class="persegi layer1 col-1" style="margin: 0 30px;">
+          <div class="lingkaran layer2" style="margin-left: 50%">
+          </div>
+          <p>Memiliki kompetensi dan keunggulan dalam alih daya SDM untuk bidang pekejaan SPG, Sales, Customer Service,
+            Administrasi, dan Teknisi dengan coverage seluruh area di Indonesia.</p>
+        </div>
+        <div class="persegi layer1 col-1" style="margin: 0 30px;">
+          <div class="lingkaran layer2" style="margin-left: 50%"></div>
+          <p>aaaa</p>
+        </div>
+        <div class="persegi layer1 col-1" style="margin: 0 30px;">
+          <div class="lingkaran layer2" style="margin-left: 50%"></div>
+          <p>aaaa</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bbody">
+    <div class="container">
+      <h1 style="align-items: center; display: flex; font-weight: 700; font-size: 50px; margin-bottom: 70px;">Product
+      </h1>
+
+
+      <input type="radio" name="slider" class="d-none" id="s1" checked>
+      <input type="radio" name="slider" class="d-none" id="s2">
+      <input type="radio" name="slider" class="d-none" id="s3">
+      <input type="radio" name="slider" class="d-none" id="s4">
+      <input type="radio" name="slider" class="d-none" id="s5">
+
+      <div class="cards">
+        <label for="s1" id="slide1">
+          <div class="card">
+            <div class="image">
+              <div>
+              </div>
+            </div>
+            <div class="infos">
+            </div>
+          </div>
+        </label>
+
+        <label for="s2" id="slide2">
+          <div class="card">
+            <div class="image">
+            </div>
+            <div class="infos">
+            </div>
+          </div>
+        </label>
+
+        <label for="s3" id="slide3">
+          <div class="card">
+            <div class="image">
+            </div>
+            <div class="infos">
+            </div>
+          </div>
+        </label>
+
+        <!-- <label for="s4" id="slide4">
+          <div class="card">
+            <div class="image">
+            </div>
+            <div class="infos">
+            </div>
+          </div>
+        </label> -->
+
+        <!-- <label for="s5" id="slide5">
+          <div class="card">
+            <div class="image">
+            </div>
+            <div class="infos">
+            </div>
+          </div>
+        </label> -->
+      </div>
+
+    </div>
+  </div>
+
+  <div class="client">
+    <div class="card-client">
+      <img class="imgcl" src="images/client.png" alt="">
+      <button class="arrow arrow-left"><img style="height: 50px; width: auto;" src="images/Arrow-left.png"></button>
+      <button class="arrow arrow-right"><img style="height: 50px; width: auto;" src="images/Arrow-right.png"></button>
+      <h1>Client Says</h1>
+      <img class="imgpp" src="images/ana.png" alt="">
+      <h2>Ana Yuniarti</h2>
+      <p class="desc">AVP HC Solution & Service</p>
+      <h1 class="kutip">“</h1>
+      <p class="isi">ISH telah menjadi partner bisnis dalam pengelolaan Sumber Daya Mitra di Telkomsat, dan menjadi
+        saksi sejarah konsolidasi bisnis satelit dari hulu ke hilir yang bermula dari 3 entitas menjadi Telkomsat.
+        Semoga kerjasama yang telah terjalin dapat saling memberikan nilai positif serta berguna bagi kedua pihak, dan
+        semakin menjadi lebih baik lagi di waktu yang akan datang. Terima kasih</p>
+      <h1 class="akh">PT Telkom Satelit Indonesia</h1>
+    </div>
+  </div>
+
+  <div class="wrapper-logo-client">
+    <h1>Our Client</h1>
+    <i id="left" class="fa-solid fa-angle-left"></i>
+    <ul class="carousel-logo-client">
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-1.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-2.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-3.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-4.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-1.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-2.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-3.png" alt="img"></div>
+      </li>
+      <li class="card-logo-client">
+        <div class="img-logo-client"><img class="imgsize" src="images/logo-client-4.png" alt="img"></div>
+      </li>
+    </ul>
+    <i id="right" class="fa-solid fa-angle-right"></i>
+  </div>
+  <div class="dot-pagination"></div>
+
+  <!-- NEWS -->
+  <div class="zzsize" style="margin-top:100px">
+    <h1 style="position: relative;
+    font-size: 40px;
+  display: flex;
+  justify-content: flex-start; margin-left: 40px;">News Update</h1>
+    <div class="containerz" style="margin-top:50px">
+      <div class="itemz itemz1">Item 1</div>
+      <div class="itemz itemz2">Item 2</div>
+      <div class="itemz itemz3">Item 3</div>
+      <div class="itemz itemz3">Item 3</div>
+      <div class="itemz itemz4">Item 4</div>
+    </div>
+  </div>
+  <!-- END NEWS -->
+
+  <!-- FOOTER -->
+  <div style="min-height: 100vh;
+position: relative;
+z-index: 1;">
+    <div class="bgfooter1">
+
+      <div
+        style="display: flex; position: relative; gap: 2em; color: white; justify-content: center; flex-direction: column; font-size: 11px;">
+        <div>
+          <h1 style="font-weight: 600; margin-top: 30%; margin-bottom: 10px;">EXPLORE</h1>
+        </div>
+        <div style="position: absolute; top: 35%; width: 625px; height: 1px; background-color: white;"></div>
+        <div style="position: absolute; top: 75%; width: 625px; height: 1px; background-color: white;"></div>
+        <div style="display: flex; gap: 4em; text-align: center;">
+          <h2>Call Us<br> +62 21 80670900</h2>
+          <h2>Open Hours<br>Monday - Friday 8:00-17:00</h2>
+          <h2>Email Address<br> info@ish.co.id</h2>
+        </div>
+        <div style="display: flex; gap:4em; margin-top: 1.5%;">
+          <h2>About</h2>
+          <h2>Products</h2>
+          <h2>News</h2>
+          <h2>Our Clients</h2>
+          <h2>Contact Us</h2>
+          <h2>Find Jobs</h2>
+        </div>
+
+      </div>
+    </div>
+    <div class="bgfooter2"></div>
+    <div class="bgfooter3"></div>
+
+
+    <div style="position: relative;">
+      <div
+        style="position: absolute; display: flex; gap: 3em; color: white; justify-content: center; flex-direction: column; font-size: 11px; position: absolute; right: 41%; bottom: -750px;">
+        <div>
+          <div class="footer-text" style="align-items: center; display: flex; flex-direction: column;">
+            <img style="height: 100px; width: auto; padding: 7%;" src="images/ishlogo-white.png" alt="">
+            <h2 style="margin-bottom: 5%;">PT Infomedia Solusi Humanika</h2>
+            <h2><i style="color: #A82A2F; margin-right: 10px;" class="fa-solid fa-location-dot"></i>Jl. Rs. Fatmawati 75. Jakarta Selatan 12150</h2>
+            <h2><i style="color: #A82A2F; margin-right: 10px;" class="fa-solid fa-phone"></i>+62 21 80670900</h2>
+            <h2><i style="color: #A82A2F; margin-right: 10px;" class="fa-solid fa-envelope"></i>info@ish.co.id</h2>
+          </div>
+        </div>
+        <div style="display: flex; gap: 2em;">
+          <div style="border-radius: 20%;" class="footerlogo"></div>
+          <div style="display: flex; border-radius: 20%; text-align: center; align-items: center;" class="footerlogo">
+            <i style="font-size: 32px; " class="fa-brands fa-linkedin"></i>
+          </div>
+          <div style="border-radius: 20%;" class="footerlogo"></div>
+        </div>
+      </div>
+    </div>
+    <div class="bgfooter4" style="text-align: center;">
+      <h4 style="margin-top: 2.5%;color: azure;">© Copyright 2023 All Rights Reserved</h4>
+    </div>
+  </div>
+  <!-- END FOOTER -->
+
+
+  <script type="text/javascript" src="js/script.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+  <script>
+    const toggleBtn = document.querySelector('.toggle_btn')
+    const toggleBtnIcon = document.querySelector('.toggle_btn i')
+    const dropDownMenu = document.querySelector('.dropdown_menu')
+
+    toggleBtn.onclick = function () {
+      dropDownMenu.classList.toggle('open')
+      const isOpen = dropDownMenu.classList.contains('open')
+
+      toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+    }
+  </script>
+
+</body>
+
+</html><?php /**PATH C:\Users\rrizq\OneDrive\Desktop\ISH 0.1 Version\ISH\resources\views/landingPage.blade.php ENDPATH**/ ?>
